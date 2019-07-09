2b443f5e379c91ed93951739d5e8fc2d7569629b
Krishna C Vangala <krishna.sairam@nike.com>
2019-07-09 11:33:42 -0700

    Fixed the finally and removed the unnessary iteration

--- src/main/java/com/athena/scim/core/SchemasValue.java
+++ src/main/java/com/athena/scim/core/SchemasValue.java
@@ -6,15 +6,15 @@ import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
+import java.sql.*;
 
 @SuppressWarnings("unchecked")
-public class SchemasValue {
+ class SchemasValue {
+    private SchemasValue(){
+        throw new IllegalStateException("Schema Class");
+    }
     private static Logger log= LogManager.getLogger(SchemasValue.class);
-    public static String getValue() {
+     static String getValue() throws SQLException {
         log.debug("Starting the Schema Value Generation method");
         DatabaseConnection dbConnection=new DatabaseConnection();
         Connection databaseConnection=dbConnection.getConnection();
@@ -24,41 +24,45 @@ public class SchemasValue {
         meta.put("created", "2010-01-23T04:56:22Z");
         meta.put("version", "1");
         meta.put("lastModified", "2011-05-13T04:42:34Z");
-        String query="select * from world.city;";
+        String query="select * from world.city limit 1;";
         JSONObject schmaJson=new JSONObject();
         JSONArray attrbArray=new JSONArray();
-        try{
             log.debug("Starting the try block");
             if(null!=databaseConnection){
+                PreparedStatement ps=null;
+                ResultSet resultSet=null;
+                try{
                 log.debug("Connection is not null");
-                PreparedStatement ps=databaseConnection.prepareStatement(query);
-                ResultSet resultSet=ps.executeQuery();
+                ps=databaseConnection.prepareStatement(query);
+                resultSet=ps.executeQuery();
                 log.info("Result set has additional value");
-                while(resultSet.next()){
-                    ResultSetMetaData resultSetMetaData= resultSet.getMetaData();
-                    log.debug("Starting to read the column Names ");
-                    for (int i = 0; i <resultSetMetaData.getColumnCount() ; i++) {
-                        JSONObject tmpJson=new JSONObject();
-                        tmpJson.put("name",resultSetMetaData.getColumnLabel(i+1));
-                        tmpJson.put("type",resultSetMetaData.getColumnTypeName(i+1));
-                        tmpJson.put("multiValued",false);
-                        tmpJson.put("description","Attribute from the table");
-                        tmpJson.put("returned","default");
-                        tmpJson.put("required",(resultSetMetaData.isNullable(i+1)));
-                        tmpJson.put("caseExact",resultSetMetaData.isCaseSensitive(i+1));
-                        tmpJson.put("mutability",!(resultSetMetaData.isWritable(i+1)));
-                        tmpJson.put("uniqueness","server");
-                        attrbArray.add(tmpJson);
-                    }
-                    break;
+                ResultSetMetaData resultSetMetaData= resultSet.getMetaData();
+                log.debug("Starting to read the column Names ");
+                for (int i = 0; i <resultSetMetaData.getColumnCount() ; i++) {
+                    JSONObject tmpJson=new JSONObject();
+                    tmpJson.put("name",resultSetMetaData.getColumnLabel(i+1));
+                    tmpJson.put("type",resultSetMetaData.getColumnTypeName(i+1));
+                    tmpJson.put("multiValued",false);
+                    tmpJson.put("description","Attribute from the table");
+                    tmpJson.put("returned","default");
+                    tmpJson.put("required",(resultSetMetaData.isNullable(i+1)));
+                    tmpJson.put("caseExact",resultSetMetaData.isCaseSensitive(i+1));
+                    tmpJson.put("mutability",!(resultSetMetaData.isWritable(i+1)));
+                    tmpJson.put("uniqueness","server");
+                    attrbArray.add(tmpJson);
+                }
+            } catch (Exception e){
+            log.error("Error occurred in catch block for the schema value generation.");
+            log.error("Error is \t"+e);
+            }finally {
+                    if(null!=ps){
+                    ps.close();}
+                    if(null!=resultSet){
+                    resultSet.close();}
                 }
             }else{
                 log.error("Connection is null nothing to do");
             }
-        }catch (Exception e){
-            log.error("Error occurred in catch block for the schema value generation.");
-            log.error("Error is \t"+e);
-        }
         schmaJson.put("Attributes",attrbArray);
         return schmaJson.toString();
     }
